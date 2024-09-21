91587a65605cd22a46ed7220043297cfe298d13a
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-21 22:34:50 +0300

    add user account api

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,15 @@ DJANGO_REST_FRAMEWORK = {
     ],
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 
 
 # Application definition
