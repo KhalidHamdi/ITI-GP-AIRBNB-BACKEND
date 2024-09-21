5152177f8c76a1ae7435309b024a4872dbdfe43b
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-21 21:03:06 +0300

    add user model

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z^0zbvvg*ap3wt#^8bo9xblpl_r&hvsn56s1odspv!bldvx3#g'
+SECRET_KEY = 'django-insecure-z^0zbvvg*ap3wt#^8bo9xblpl_r&hvsn56s1odspv!bldvx3#g' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
