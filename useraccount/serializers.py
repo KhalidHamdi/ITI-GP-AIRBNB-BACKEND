a7ad5d43c61764353c439ba9e6e697a1a2bcb1dd
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-26 21:46:50 +0300

    edit: serializers.py

--- useraccount/serializers.py
+++ useraccount/serializers.py
@@ -1,11 +1,39 @@
 from rest_framework import serializers
-
-from property.serializers import PropertiesListSerializer
+from dj_rest_auth.serializers import LoginSerializer as DefaultLoginSerializer
 from .models import User
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            'id', 'name', 'avatar_url'
-        )
+        fields = ('id', 'name', 'avatar_url')
+
+class CustomLoginSerializer(DefaultLoginSerializer):
+    username = None  # Exclude the username field
+    email = serializers.EmailField(required=True)
+
+    def authenticate(self, **kwargs):
+        from django.contrib.auth import authenticate
+        return authenticate(**kwargs)
+
+    def get_fields(self):
+        fields = super().get_fields()
+        fields['email'] = serializers.EmailField(required=True)
+        return fields
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = self.authenticate(email=email, password=password)
+            if user:
+                if not user.is_active:
+                    raise serializers.ValidationError('User account is disabled.')
+                # Authentication successful
+            else:
+                raise serializers.ValidationError('Unable to log in with provided credentials.')
+        else:
+            raise serializers.ValidationError('Must include "email" and "password".')
+
+        attrs['user'] = user
+        return attrs
