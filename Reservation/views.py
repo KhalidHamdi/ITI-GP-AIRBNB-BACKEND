f839851399dbc7a3b0f0ac8f7f043577a2a9e6f2
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-28 11:49:34 +0300

    add: views.py to Reservation app

--- Reservation/views.py
+++ Reservation/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# Reservation/views.py
+from rest_framework import generics, permissions
+from .models import Reservation
+from .serializers import ReservationSerializer
+from property.models import Property
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class ReservationListView(generics.ListAPIView):
+    serializer_class = ReservationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        property_id = self.kwargs.get('property_id')
+        property_obj = get_object_or_404(Property, id=property_id)
+        return Reservation.objects.filter(property=property_obj)
