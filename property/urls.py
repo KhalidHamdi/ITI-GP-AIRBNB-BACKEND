bcc25a54179943900e9d06d860c04d8f950f6047
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-28 11:12:32 +0300

    add: URL Pattern for Geocode Endpoint

--- property/urls.py
+++ property/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import GeocodeView
 
 from . import api
 
@@ -7,5 +8,9 @@ urlpatterns = [
     path('', api.properties_list, name='api_properties_list'),
     path('create/', api.create_property, name='api_create_property'),
     path('<uuid:pk>/', api.properties_detail, name='api_properties_detail'),
+    
+    # URL Pattern for Geocode Endpoint :) 
+    path('geocode/', GeocodeView.as_view(), name='geocode'),
+
 
 ]
