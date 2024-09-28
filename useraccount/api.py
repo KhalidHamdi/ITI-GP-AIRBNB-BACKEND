e655d30a8d4b1f80f84ac48689071ab46cd4e37e
BasmalaSalem <basmala.mahmoud.salem@gmail.com>
2024-09-28 21:44:37 +0300

    add HostDetails endpoint

--- useraccount/api.py
+++ useraccount/api.py
@@ -48,8 +48,9 @@ class RegisterView(APIView):
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
-def landlord_detail(request, pk):
-    user = User.objects.get(pk=pk)
+def landlord_detail(request, username):
+    user = User.objects.get(username=username)
+    print("User", user)
 
     serializer = UserDetailSerializer(user, many=False)
 
