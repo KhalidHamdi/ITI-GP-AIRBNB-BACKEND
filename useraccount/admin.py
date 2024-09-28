2eb28761667ff86a2cb835434e4d37d47bab1df2
khalidhamdi <khalidhamdi204@gmail.com>
2024-09-28 04:33:43 +0300

    test

--- useraccount/admin.py
+++ useraccount/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
+from .models import CustomUserManager 
 
-admin.site.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('username', 'avatar')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
--- useraccount/serializers.py
+++ useraccount/serializers.py
@@ -1,11 +1,47 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
-from property.serializers import PropertiesListSerializer
-from .models import User
+
+User = get_user_model()
 
 class UserDetailSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
     class Meta:
         model = User
-        fields = (
-            'id', 'name', 'avatar_url'
+        fields = ['username', 'email', 'password1', 'password2']
+        extra_kwargs = {
+            'username': {'required': True, 'min_length': 1, 'max_length': 20},
+            'email': {'required': True},
+        }
+
+    def validate_username(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Username cannot be blank.")
+        if len(value) > 20:
+            raise serializers.ValidationError("Username cannot exceed 20 characters.")
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with that username already exists.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError({"password2": "Passwords do not match."})
+        return data
+
+    def create(self, validated_data):
+        password = validated_data.pop('password1')
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=password
         )
+        return user
