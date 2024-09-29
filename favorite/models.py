259494c5df3d1a38f166f1297d78daade7741998
khalidhamdi <khalidhamdi204@gmail.com>
2024-09-29 11:12:52 +0300

    feat: add favorite app

---
+++ favorite/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from useraccount.models import User
+from property.models import Property
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, related_name='favorites', on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, related_name='favorited_by', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'property')
+
+    def __str__(self):
+        return f"{self.user} favorited {self.property}"
