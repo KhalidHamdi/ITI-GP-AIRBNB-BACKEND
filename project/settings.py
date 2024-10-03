88c3e0198334a978c3ea6aa30c4584e30a069cd0
David Emad <demad2527@gmail.com>
2024-10-03 05:29:43 +0300

    csrf

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ['.vercel.app']
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS=['https://itnb.up.railway.app']
 
 WEBSITE_URL='http://localhost:5173'
 
