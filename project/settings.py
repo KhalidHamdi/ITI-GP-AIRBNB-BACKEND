2b86d116fb682973136e53864b8e8c21a8a2fcb5
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-10-11 16:56:04 +0300

    fix the deploy issue1

--- project/settings.py
+++ project/settings.py
@@ -36,8 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-z^0zbvvg*ap3wt#^8bo9xblpl_r&hvsn56s1odspv!bldvx3#g' 
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY='django-insecure-z^0zbvvg*ap3wt#^8bo9xblpl_r&hvsn56s1odspv!bldvx3#g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
